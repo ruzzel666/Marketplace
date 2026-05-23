5941817cd41a5d932e2e41afa42c45830811b347
Georgy Zaitsev <gnz29042003@mail.ru>
2026-05-23 16:56:13 +0500

    Updates

--- _nuxt/B9FRn3Js.js
+++
@@ -1,1 +0,0 @@
-import{X as a,Y as s,p as r,s as u,r as o}from"./bDbqXfKN.js";function d(t){const e=t||s();return e.ssrContext?.head||e.runWithContext(()=>{if(r()){const n=u(o);if(!n)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return n}})}function i(t,e={}){const n=e.head||d(e.nuxt);return a(t,{head:n,...e})}export{i as u};
